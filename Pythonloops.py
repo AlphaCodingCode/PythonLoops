f8f2acaa9e2eff4932d4addfa8be6ace06407166
AlphaCodingCode <52750549+AlphaCodingCode@users.noreply.github.com>
2019-07-16 15:32:59 -0400

    Add files via upload

---
+++ Pythonloops.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.color("red")
+turtle.done()
+
+# for i in range(4):
+#   turtle.forward(150)
+#   turtle.right(90)
+# turtle.done()
